f35fb1716b14d6668949597e51793f9a8ccc144b
Oliver Stenbom <oliver@stenbom.eu>
2017-06-06 15:21:51 +0200

    Base flask app

---
+++ application.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+SECRET_KEY = 'development key'
+TEMPLATES_AUTO_RELOAD = True
+
+application = Flask(__name__)
+
+@application.route('/')
+def index():
+    return render_template('base.html')
+
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
